9a30a977db861f595847badce2b17b1a170ce92a
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-22 04:21:43 -0700

    moves next_state() from state to statemachine

--- src/statemachine.rs
+++ src/statemachine.rs
@@ -11,21 +11,6 @@ pub enum State {
     Error
 }
 
-impl State {
-
-    // retrieve the next state 
-    fn next(&self) -> Result<State,JSPTemplateError> {
-        match self {
-            State::Start => Ok(State::RegexParsing),
-            State::RegexParsing => Ok(State::NodeParsing),
-            State::NodeParsing => Ok(State::EdgeParsing),
-            State::EdgeParsing => Ok(State::Done),
-            State::Done => Err(JSPTemplateError::NoValidNextState(State::Done)),
-            State::Error => Err(JSPTemplateError::NoValidNextState(State::Error))
-        }
-    }
-}
-
 pub struct StateMachine {
     state: State,
     parsers: (
@@ -47,8 +32,10 @@ impl StateMachine {
     /// Parse teh current line of input. If the input is a Header, transition
     /// the statemachine to the next valid state.
     pub fn parse(&mut self, input: &str) -> Result<ParseResult, JSPTemplateError> {
-
-        let cstate = match self.state {
+        // parse current line if the statemachine is in a state that has a parser
+        // associated with it. If the state doesnt have an associated parser, set
+        // the appropriate error.
+        let parsed_line = match self.state {
             State::Start        => Ok(self.parsers.0(input)),
             State::RegexParsing => Ok(self.parsers.1(input)),
             State::NodeParsing  => Ok(self.parsers.2(input)),
@@ -56,16 +43,18 @@ impl StateMachine {
             State::Done  => Err(JSPTemplateError::DoneState),
             State::Error => Err(JSPTemplateError::ErrorState),
         };
-
-        match cstate {
+        // outer result determines whether the statemachine is in a state 
+        // that can parse lines. Neither the Done state nor the Error state 
+        // qualify as such.
+        match parsed_line {
             Ok(result) => {
-                // grabbing from IResult
-                //if let Ok((_, value)) = result {
+                // inner Result determines whether parsing of line is ok
                 match result {
                     Ok((_, value)) => {
                         if let ParseResult::Header(ref new_state) = value {
                             let current_state = self.state.clone();
-                            let next_state = self.state.next()?;
+                            // get the expected next state from the
+                            let next_state = self.next_state()?;
                             let new_state = match new_state {
                                 Header::Node  =>  State::NodeParsing,
                                 Header::Edge  =>  State::EdgeParsing,
@@ -76,7 +65,7 @@ impl StateMachine {
                             if next_state != new_state {
                                 return Err(JSPTemplateError::InvalidStateTransition(current_state, new_state))   
                             }
-
+                            // set the new state if the transition is a valid one to make
                             self.state = new_state;
                         }
 
@@ -88,6 +77,18 @@ impl StateMachine {
                 }
             }, 
             Err(e) =>    Err(e),
+        }
+    }
+
+    // retrieve the next state in the statemachine given the current state
+    fn next_state(&self) -> Result<State, JSPTemplateError> {
+        match self.state {
+            State::Start        => Ok(State::RegexParsing),
+            State::RegexParsing => Ok(State::NodeParsing),
+            State::NodeParsing  => Ok(State::EdgeParsing),
+            State::EdgeParsing  => Ok(State::Done),
+            State::Done         => Err(JSPTemplateError::NoValidNextState(State::Done)),
+            State::Error        => Err(JSPTemplateError::NoValidNextState(State::Error))
         }
     }
 }
